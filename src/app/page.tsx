a418c083791122e2a46aab9b92863976c6cbdfa4
joanasofia03 <joanasofia2112@gmail.com>
2025-05-06 12:17:53 +0100

    - alteracao do favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className="w-full overflow-hidden flex items-center justify-center flex-col bg-gray-200">
       Info
+      <Head>
+        <title>Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     </div>
   );
 }
